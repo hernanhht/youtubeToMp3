fad2d6fc75c3d52dc5b82b887fbe468df10118f9
Hernán Tierno <htierno@gmail.com>
2014-08-08 16:24:25 -0300

    Added file download

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 var querystring     = require("querystring");
 var request         = require("request");
 var async           = require("async");
+var http            = require('http');
+var fs              = require('fs');
 
 var regex = "href=\"/watch([^\"]*)\"";
-var youTubeUrl = "https://www.youtube.com";
+var youTubeUrl = "http://www.youtube.com";
+var youTubeToMp3URL = "http://youtubeinmp3.com/fetch/?video=";
 
-var searchArray = ["", ""];
+var searchArray = [];
 
 var searchInYouTube = function (searchString, cb) {
     var youTubeSearchUrl = youTubeUrl + "/results?" + querystring.stringify({"search_query": searchString});
@@ -19,13 +22,35 @@ var searchInYouTube = function (searchString, cb) {
             url;
 
         url = path.replace("href=\"", youTubeUrl).replace("\"", "");
-        return cb(null, url);
+        return cb(null, {searchString: searchString, url: url});
+    });
+};
+
+var downloadMp3 = function (result, cb) {
+    var file = fs.createWriteStream(result.searchString + ".mp3");
+
+    request(youTubeToMp3URL + result.url, function (err, response, fileUrl) {
+        if (err) { return cb(err); }
+
+        http.get(fileUrl, function (res) {
+            console.log("Downloading MP3 for: " + result.searchString);
+            res.pipe(file);
+
+            res.on('end', function () {
+                cb();
+            });
+        });
     });
 };
 
 async.map(searchArray, searchInYouTube, function (err, results) {
     if (err) { throw err; }
-    console.log(results);
+
+    async.each(results, downloadMp3, function (err) {
+        if (err) { throw err; }
+
+        console.log("All files downloaded");
+    });
 });
 
 
